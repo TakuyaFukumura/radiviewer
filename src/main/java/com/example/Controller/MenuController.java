26f38d329543ef650c8b8d0656f11ce1e2a077e7
TakuyaFukumura <blackchoco1114@gmail.com>
2021-01-23 15:17:27 +0900

    メニュー用コントローラ作成

---
+++ src/main/java/com/example/Controller/MenuController.java
@@ -0,0 +1,51 @@
+/**
+ *
+ */
+package com.example.Controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.Dto.DividendDto;
+import com.example.Logic.TableLogic;
+
+/**
+ * @author fukumura
+ *
+ */
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+	TableLogic tableLogic = new TableLogic();
+
+	@Autowired
+	HttpSession session;
+
+	/*
+	 * 参考ページ
+	 * https://qiita.com/nvtomo1029/items/316c5e8fe5d0cd92339c
+	 * https://qiita.com/misskabu/items/81fa2c774f92c63125b5
+	 */
+	@PostMapping
+	public String index(@RequestParam("csv_file")MultipartFile csv_file, Map<String, Object> model) {
+		List<DividendDto> contents = tableLogic.fileContents(csv_file);
+		session.setAttribute("dividendDtoList", contents);
+		return "menu";
+	}
+
+	@GetMapping
+	public String indexGet( Map<String, Object> model) {
+		//データがあるかどうかチェック
+		return "menu";
+	}
+}
