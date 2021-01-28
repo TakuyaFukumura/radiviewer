92bd721c197f3dd78937b22affe59df9469b7efe
TakuyaFukumura <blackchoco1114@gmail.com>
2021-01-28 22:15:38 +0900

    データの有無に応じたリダイレクト設定追加

--- src/main/java/com/example/Controller/MenuController.java
+++ src/main/java/com/example/Controller/MenuController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,12 +36,19 @@ public class MenuController {
 	public String index(@RequestParam("csv_file") MultipartFile csv_file, Map<String, Object> model) {
 		List<DividendDto> contents = menuLogic.fileContents(csv_file);
 		session.setAttribute("dividendDtoList", contents);
+		if(CollectionUtils.isEmpty(contents)) { //データがあるかどうかチェック
+			return "redirect:/";
+		}
 		return "menu";
 	}
 
 	@GetMapping
-	public String indexGet(Map<String, Object> model) {
-		//データがあるかどうかチェック
+	public String index(Map<String, Object> model) {
+		@SuppressWarnings("unchecked")
+		List<DividendDto> contents = (List<DividendDto>) session.getAttribute("dividendDtoList");
+		if(CollectionUtils.isEmpty(contents)) {
+			return "redirect:/";
+		} //本当はフィルターで実装したい
 		return "menu";
 	}
 }
